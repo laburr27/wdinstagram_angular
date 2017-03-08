f0705230ca4ecce4dc64bda69f92a751c5fab2a0
Lisa Anne Burr <burr_lisa@bah.com>
2017-03-08 16:31:00 -0500

    initial commit; created basic app

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 "use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+let wdinstagramData = [
+  { title: "Hey", author: "Me", img_url:"http://media-cache-ak0.pinimg.com/736x/02/b9/4e/02b94e2ce1c16f5a61a4f0cf249e5919.jpg" },
+  { title: "Howdy", author: "Me Again", img_url:"https://img.memesuper.com/8961722642cca2b10c217d1646e850a6_star-trek-tng-memes-star-trek-tng-memes_1280-961.jpeg" },
+  { title: "Aloha", author: "Yep.  Me.", img_url:"http://img14.deviantart.net/c3ed/i/2015/181/4/a/star_trek_tng_hd_meme_05_by_gutgutgut-d8zft7s.jpg" },
+]
+
+angular
+  .module("wdinstagramApp", [
+    "ui.router"
+  ])
+  .controller("wdinstagramCtrl", [ wdinstagramController])
+
+  function wdinstagramController () {
+    this.wdinstagrams = wdinstagramData;
+  }
